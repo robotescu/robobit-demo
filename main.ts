04f1ed8b6728c7926b36b436370ce2ebfd3a9860
robotescu <80849623+robotescu@users.noreply.github.com>
2024-06-10 12:16:31 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 robobit.select_model(RBModel.Mk3)
 basic.showIcon(IconNames.Asleep)
+let v_st = 20
+let v_dr = 50
 basic.forever(function () {
     while (robobit.sonar(RBPingUnit.Centimeters) >= 5) {
-        robobit.move(RBMotor.Left, RBDirection.Forward, 30)
-        robobit.move(RBMotor.Right, RBDirection.Forward, 60)
-        basic.pause(500)
+        robobit.move(RBMotor.Left, RBDirection.Forward, v_st)
+        robobit.move(RBMotor.Right, RBDirection.Forward, v_dr)
+        basic.pause(1000)
+        v_st += 5
+        v_dr += 5
     }
     robobit.stop(RBStopMode.Coast)
     while (robobit.sonar(RBPingUnit.Centimeters) <= 5) {
